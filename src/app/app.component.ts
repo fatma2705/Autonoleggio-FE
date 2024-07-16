c40729bea5278f794b25eb4723cacf65eaddd5ee
fatma2705 <arafatma2003@gmail.com>
2024-07-16 17:27:07 +0200

    footer e navbar aggiunti

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./auth/login/login.component"
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent,CommonModule],
+  imports: [RouterOutlet, LoginComponent, CommonModule, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/user-home/user-home.component.ts
+++ src/app/components/user-home/user-home.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { CarSearchComponent } from "../car/car-search/car-search.component";
-import { PrenotazioniComponent } from "../prenotazioni/prenotazioni.component";
 import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-user-home',
   standalone: true,
-  imports: [CarSearchComponent, PrenotazioniComponent],
+  imports: [ ],
   templateUrl: './user-home.component.html',
   styleUrl: './user-home.component.css'
 })
